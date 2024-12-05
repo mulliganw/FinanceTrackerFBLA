f846e5a6682908888fb505ef22a219bcf5cbf769
William Mulligan <mulliganw26@hsnet.ahsd.org>
2024-12-05 02:47:45 +0000

    Update many functions with more in-depth features so that everything works as intended. Comment code.

--- main.py
+++ main.py
@@ -2,76 +2,80 @@ import pandas as pd
 from typing import *
 
 class Database :
+    # when a database is created, check to see if it already exists. if it does, read it, if it doesn't, create it
     def __init__(self, filename, columns):
         self.filename = filename
         self.columns = columns
+        # attempt to read a stored csv with the requested filename
         try: 
             self.df = pd.read_csv(filename).drop(['Unnamed: 0'],axis=1)
+        # if it doesn't work, say so and make a new DataFrame for it
         except:
             self.df = pd.DataFrame(columns=columns)
             print('failed to read csv')
-            try: 
-                self.df.to_csv(filename, mode='w')
-            except:
-                self.df.to_csv(filename, mode='x')
+            # create a new csv with the requested filename since it doesn't exist
+            self.df.to_csv(filename, mode='x')
         self.current_index = len(self.df)
     
     def get_index(self) :
         return len(self.df)
     
+    # adds a new row to a database instance
     def write_row(self, data, cols) :
+        # make sure the length of cols is greater than the length of the data so there's no errors
         if(len(cols) < len(data)) : 
             return "Excessive data entered; did you make sure there was less data than there was columns?"
-        index = len(self.df)
+        index = self.get_index()
+        # TODO: change cols and data to be payload and as a dict
         for i in range(0, len(cols)) :
             self.df.loc[index, str(cols[i])] = data[i]
-        self.current_index += 1
         self.df.to_csv(self.filename, mode='w')   
 
+    # TODO: implement
     def remove_row() :
         return 0
 
-    def delete_user(self, id):
-        self.df.drop(id)
-        self.df.sort_index()
-        self.df.to_csv(self.filename, mode='w')
-
 # Create the three main databases
-# TODO: Move database class above everything else and define these beforehand so I don't have to take in 
-# db as a param
 users = Database('Users.csv', ['user_id', 'username'])
 accounts = Database('Accounts.csv', ['account_id', 'user', 'balance', 'account_type'])
-transactions = Database('Transactions.csv', ['user', 'amount', 'location', 'time'])
+transactions = Database('Transactions.csv', ['user', 'amount', 'time'])
 
+class User : 
+    def __init__(self, username) :
+        self.id = users.get_index()
+        self.username = username
+        # write all the info to the database
+        users.write_row([self.id, self.username], users.columns)  
+        self.transactions = []
+        self.accounts = []
+
+    # The following two methods are abstracted to the User class so that a user is included by default.
+    def make_transaction(self, amount, time=None) :
+        transaction = Transaction(self, amount, time)
+        self.transactions.append(transaction)
+        return transaction
+    
+    def create_account(self, balance=0, type='checking') :
+        account = Account(self, balance, type)
+        self.accounts.append(account)
+        return account
+
+# TODO: update this to immediately add it to the accounts db
 class Transaction :
-    total_transactions = 0
-    def __init__(self, user, amount, location=None, time=None) :
+    def __init__(self, user, amount, time=None) :
         self.id = transactions.get_index()
         self.user = user
         self.amount = amount
         self.time = time
         self.location = location
 
-class User : 
-    def __init__(self, username) :
-        self.id = users.get_index()
-        self.username = username
-        users.write_row([self.id, self.username], users.columns)  
-        # These should be their own dataframes in the future.     
-        self.transactions = []
-        self.accounts = []
-
-    def make_transaction(self, amount, item=None, location=None) :
-        self.transactions.append(Transaction(amount, item, location))
-        
-    def create_account(self) :
-        self.accounts.append(Account(self.id))
-
 class Account : 
-    def __init__(self, user, balance=0, type='checking') :
+    # TODO: update this to immediately add it to the accounts db
+    def __init__(self, user, balance, type) :
         self.id = accounts.get_index()
         self.user = user
         self.balance = balance
         self.type = type
 
-billy = User('Billy', users)
+# TODO: set up test cases with simple terminal input
+billy = User('Billy')
